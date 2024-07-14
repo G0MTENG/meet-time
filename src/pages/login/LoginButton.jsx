fdc6b013d0434e09a4f42ab9b91b26b74b47ad9d
mobetus <tkfmkim99@naver.com>
2024-07-14 22:16:38 +0900

    FEAT: Create select week page and login page again based on master

---
+++ src/pages/login/LoginButton.jsx
@@ -0,0 +1,5 @@
+import styles from '@/styles/login/LoginInput.module.css'
+
+export default function LoginButton() {
+  return <div className={styles.loginbutton}>로그인</div>
+}
--- src/pages/select/week/WeekSelectPage.jsx
+++ src/pages/select/week/WeekSelectPage.jsx
@@ -1,3 +1,15 @@
-export default function WeekSelectPage() {
-  return <div>WeekSelectPage</div>
+import styles from '@/styles/select/DateSelectPage.module.css'
+
+export default function SelectWeek() {
+  return (
+    <div className={styles.container}>
+      <div>월요일</div>
+      <div>화요일</div>
+      <div>수요일</div>
+      <div>목요일</div>
+      <div>금요일</div>
+      <div>토요일</div>
+      <div>일요일</div>
+    </div>
+  )
 }
