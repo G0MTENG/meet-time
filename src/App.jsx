3a62071f0dd5ed7a36c69e82894057c62f665ebe
g0mteng <gomteng03@naver.com>
2024-07-16 11:23:12 +0900

    Feat: timeSelect Page Domain setting

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from '@/styles/App.module.css'
 import HomePage from '@/pages/home/HomePage'
 import LoginPage from '@/pages/login/LoginPage'
 import SelectPage from '@/pages/select/SelectPage'
+import TimeSelectPage from './pages/timeSelect/timeSelectPage'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route index element={<HomePage />}></Route>
         <Route path="select/*" element={<SelectPage />}></Route>
         <Route path="login" element={<LoginPage />}></Route>
+        <Route path="timeSelect" element={<TimeSelectPage />}></Route>
       </Routes>
     </div>
   )
---
+++ src/pages/timeSelect/timeSelectPage.jsx
@@ -0,0 +1,3 @@
+export default function TimeSelectPage() {
+  return <div>timeSelectPage</div>
+}
