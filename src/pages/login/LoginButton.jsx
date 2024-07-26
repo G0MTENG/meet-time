a823abcbc58c1293c1672a0f45255088c8fb7256
mobetus <tkfmkim99@naver.com>
2024-07-26 16:30:26 +0900

    FEAT: 시간&로그인 버튼 수정

--- src/pages/login/LoginButton.jsx
+++ src/pages/login/LoginButton.jsx
@@ -1,5 +1,5 @@
 import styles from '@/styles/components/Button.module.css'
 
 export default function Button({ children }) {
-  return <div className={styles.loginbutton}>{children}</div>
+  return <div className={styles.button}>{children}</div>
 }
