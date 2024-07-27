523c9c80103181ef593e345650be202b72f6b768
g0mteng <gomteng03@naver.com>
2024-07-27 14:48:02 +0900

    Fix: input error handling

--- src/pages/join/JoinPage.jsx
+++ src/pages/join/JoinPage.jsx
@@ -8,13 +8,21 @@ import { useState } from 'react'
 export default function JoinPage() {
   const navigate = useNavigate()
   const [groupInput, setGroupInput] = useState('')
+  const [error, setError] = useState('')
+
+  const errorMsg = '잘못된 입력입니다. 다시 시도해주세요.'
 
   const handleJoinClick = () => {
-    if (!groupInput.includes('#')) {
+    if (!groupInput) {
       return
     }
 
     const [_groupName, _tag] = groupInput.split('#')
+    if (!_tag || !_groupName) {
+      setError(errorMsg)
+      return
+    }
+
     navigate(`/login?group=${_groupName}&tag=${_tag}`)
   }
 
@@ -25,6 +33,7 @@ export default function JoinPage() {
       </div>
       <p>모임이름과 Tag를 입력해주세요!</p>
       <JoinInput value={groupInput} set={setGroupInput} />
+      <div className={styles.error}>{error}</div>
       <Button onClick={handleJoinClick}>모입 입장</Button>
     </div>
   )
