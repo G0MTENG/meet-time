d5908e35621191a0fec9ed251c6470bb4d7fda61
g0mteng <gomteng03@naver.com>
2024-08-01 21:22:53 +0900

    Rename: mock apis rename

--- src/mocks/apis/join.js
+++ src/mocks/apis/join.js
@@ -1,16 +1,16 @@
 import { http, HttpResponse } from 'msw'
 
-const postExample3 = http.post('/meettime/join', () => {
+const postJoin = http.post('/meettime/join', () => {
   return HttpResponse.json({
     meeting_id: 1,
     meeting_title: '해커톤',
     meeting_type: 'D',
-    meeting_list: '[2024 - 07 - 29, 2024 - 07 - 30, 2024 - 07 - 31]',
+    meeting_list: ['2024-07-29', '2024-07-30', '2024-07-31'],
     meeting_day_id: [34, 35, 36],
-    meeting_start_time: '09 : 00',
-    meeting_end_time: '12 : 00',
+    meeting_start_time: 18,
+    meeting_end_time: 24,
     meeting_create_time: ' 2024-07-29T12:00:00',
   })
 })
 
-export { postExample3 }
+export { postJoin }
--- src/mocks/apis/week-create.js
+++ src/mocks/apis/week-create.js
@@ -1,16 +1,16 @@
 import { http, HttpResponse } from 'msw'
 
-const postExample2 = http.post('./meettime/week/create', () => {
+const postWeekCreate = http.post('./meettime/week/create', () => {
   return HttpResponse.json({
     meeting_id: 10,
     meeting_title: '멋사',
     meeting_type: 'W',
-    meeting_list: ['monday', 'sunday'],
+    meeting_list: [0, 6],
     meeting_day_id: [40, 41],
-    meeting_start_time: '09 : 00',
-    meeting_end_time: '12 : 00',
+    meeting_start_time: 18,
+    meeting_end_time: 24,
     meeting_create_time: ' 2024-07-29T12:00:00',
   })
 })
 
-export { postExample2 }
+export { postWeekCreate }
