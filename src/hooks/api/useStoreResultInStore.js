74a1fc4d085ec0f25764b2b878fc744514e05203
g0mteng <gomteng03@naver.com>
2024-08-06 00:17:11 +0900

    Feat: API Change result

--- src/hooks/api/useStoreResultInStore.js
+++ src/hooks/api/useStoreResultInStore.js
@@ -1,4 +1,5 @@
 import { useResultStore } from '@/stores/resultStore'
+import { GROUPTYPE } from '@/utils/groupType'
 
 export const useStoreResultInStore = () => {
   const {
@@ -18,14 +19,19 @@ export const useStoreResultInStore = () => {
       return
     }
 
-    setMeetingId(data?.meeting_id)
-    setMeetingTitle(data?.meeting_title)
-    setMeetingList(data?.meeting_list)
-    setMeetingDayId(data?.meeting_day_id)
-    setMeetingStartTime(data?.meeting_start_time)
-    setMeetingEndTime(data?.meeting_end_time)
-    setAllPeople(data?.all_people)
-    setPossibleList(data?.possible_list)
-    setMeetingType(data?.meeting_type)
+    if (data.meeting_type === GROUPTYPE.DATE) {
+      setMeetingList(data.meeting_day_list)
+    } else {
+      setMeetingList(data.meeting_week_list)
+    }
+
+    setMeetingId(data.meeting_id)
+    setMeetingTitle(data.meeting_title)
+    setMeetingDayId(data.meeting_day_id)
+    setMeetingStartTime(data.meeting_start_time)
+    setMeetingEndTime(data.meeting_end_time)
+    setAllPeople(data.all_people)
+    setPossibleList(data.possible_list)
+    setMeetingType(data.meeting_type)
   }
 }
