09398d9ca5e91de3ae1eadce2493c3ec38975453
g0mteng <gomteng03@naver.com>
2024-08-05 12:41:21 +0900

    Fix: meetingTitle typo error solve

--- src/components/LogoBox.jsx
+++ src/components/LogoBox.jsx
@@ -4,14 +4,15 @@ import LinkCopy from '@/components/LinkCopy'
 import { useGroupPersistStore } from '@/stores/groupPersistStore'
 
 export default function LogoBox() {
-  const { meetingTite } = useGroupPersistStore()
+  const { meetingTitle } = useGroupPersistStore()
+  console.log(meetingTitle)
 
   return (
     <div className={styles.container}>
       <div className={styles.logoBox}>
         <SelectLogo />
       </div>
-      <div className={styles.group}>{meetingTite}</div>
+      <div className={styles.group}>{meetingTitle}</div>
       <div className={styles.link}>
         <LinkCopy />
       </div>
