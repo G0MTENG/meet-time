94de5828d5e083a1c610e75e76583e5d382dd989
g0mteng <gomteng03@naver.com>
2024-08-06 19:55:03 +0900

    Feat: Calendar string format defined

--- src/utils/dateHelper.js
+++ src/utils/dateHelper.js
@@ -3,7 +3,9 @@ const weekly = ['', '월', '화', '수', '목', '금', '토', '일']
 
 const getDate = date => {
   const year = date.getYear() + 1900
-  return [year, date.getMonth() + 1, date.getDate()]
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  return [year, month, day]
 }
 
 const getYearMonthDate = date => {
